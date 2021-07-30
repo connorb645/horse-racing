2aa4fdea75c00925d62469e06d63fdef75a6258f
Connor Black <connor.black@notifytechnology.com>
2021-07-30 18:01:04 +0100

    - Initial project creation

---
+++ Horse RacingTests/Horse_RacingTests.swift
@@ -0,0 +1,13 @@
+//
+//  Horse_RacingTests.swift
+//  Horse RacingTests
+//
+//  Created by Connor Black on 30/07/2021.
+//
+
+import XCTest
+@testable import Horse_Racing
+
+class Horse_RacingTests: XCTestCase {
+
+}
